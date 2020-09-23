fadd93cf6ed480de7d82b4afe652448f41fd1cc9
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-09-23 14:48:02 +0530

    Add header component with spacex logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div className="header-container">
+			<div className="logo-container">
+				<img src="../spacex-logo.png" alt="spacex-logo" />
+			</div>
+		</div>
+	);
+}
+
+export default Header;
