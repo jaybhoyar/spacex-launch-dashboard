ab57198360d68a261a490c8d750d1fb71294b9fd
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-09-24 10:09:43 +0530

    Add Datepicker and launch status filter

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Dropdown, Form } from "semantic-ui-react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const launchTimelines = [
 	{
@@ -19,32 +21,81 @@ const launchTimelines = [
 	},
 ];
 
+const launchStatuses = [
+	{
+		key: "All Launches",
+		text: "All ",
+		value: "All",
+	},
+	{
+		key: "Successful",
+		text: "Successful",
+		value: "Successful",
+	},
+	{
+		key: "Failed",
+		text: "Failed",
+		value: "Failed",
+	},
+];
 function Dashboard() {
 	return (
 		<>
 			<div className="dashboard-container">
 				<div className="filters-container">
 					<div className="timeline-filter">
-						<Form>
-							<Form.Field className="text-align-center">
-								<label className="filter-name-label">
-									Launch Timeline
-								</label>
-								<Dropdown
-									placeholder="Select Launch Timeline"
-									fluid
-									selection
-									options={launchTimelines}
-								/>
-							</Form.Field>
-						</Form>
+						<p className="filter-name-label text-align-center">
+							Launch Timeline
+						</p>
+						<Dropdown
+							placeholder="Select Launch Timeline"
+							fluid
+							selection
+							options={launchTimelines}
+						/>
 					</div>
 					<div className="multiple-filters">
 						<div className="date-filter">
-							<div className="date start-date"></div>
-							<div className="date end-date"></div>
+							<Form>
+								<Form.Group>
+									<Form.Field className="text-align-center">
+										<label className="filter-name-label">
+											Start Date
+										</label>
+										<DatePicker
+											selectsStart
+											isClearable
+											showYearDropdown
+											dateFormat="yyyy/MM/dd"
+											placeholderText="Start Date"
+										/>
+									</Form.Field>
+									<Form.Field className="text-align-center">
+										<label className="filter-name-label">
+											End Date
+										</label>
+										<DatePicker
+											selectsStart
+											isClearable
+											showYearDropdown
+											dateFormat="yyyy/MM/dd"
+											placeholderText="End Date"
+										/>
+									</Form.Field>
+								</Form.Group>
+							</Form>
 						</div>
-						<div className="status-filter"></div>
+						<div className="status-filter">
+							<p className="filter-name-label text-align-center">
+								Launch Status
+							</p>
+							<Dropdown
+								placeholder="Select Launch Status"
+								fluid
+								selection
+								options={launchStatuses}
+							/>
+						</div>
 					</div>
 				</div>
 				<div className="table-container"></div>
