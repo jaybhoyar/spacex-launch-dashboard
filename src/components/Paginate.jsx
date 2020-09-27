0e3af3d5165df6a7eb77a2570a9dc8604abf0957
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-09-27 13:40:02 +0530

    Add Pagination functionality with launch count

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { Pagination } from "semantic-ui-react";
 
-function Paginate() {
+function Paginate({ launches, activePage, setActivePage, launchCount }) {
+	const handlePageChange = (e, { activePage }) => {
+		setActivePage(activePage);
+	};
 	return (
-		<div className="pagination-container">
-			<Pagination
-				boundaryRange={1}
-				activePage={1}
-				size="tiny"
-				siblingRange={1}
-				totalPages={10}
-				firstItem={null}
-				lastItem={null}
-				prevItem={null}
-				nextItem={null}
-			/>
-		</div>
+		<>
+			{launchCount > 10 ? (
+				<div className="pagination-container">
+					<Pagination
+						boundaryRange={1}
+						size="tiny"
+						siblingRange={1}
+						totalPages={Math.ceil(launchCount / 10)}
+						onPageChange={handlePageChange}
+						activePage={activePage}
+						firstItem={null}
+						prevItem={null}
+						nextItem={null}
+					/>
+				</div>
+			) : (
+				""
+			)}
+		</>
 	);
 }
 
